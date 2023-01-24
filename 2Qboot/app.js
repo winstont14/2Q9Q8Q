e34f5706a596f8c0e4bffec2f3a2f4c80cbca763
winstont14 <anandaton000@gmail.com>
2023-01-24 21:04:24 +0700

    add if not tick

--- 2Qboot/app.js
+++ 2Qboot/app.js
@@ -3,16 +3,21 @@ function processQuestionnaire()
                 var form = document.getElementById("questionnaire-form");
                 var q1 = form.elements["q1"].value;
                 var q2 = form.elements["q2"].value;
-
+                if (!q1 || !q2) {
+                    // display error message to user
+                     swal("โปรดเลือกข้อคำถามให้ครบ", "", "error");
+                     document.getElementById("processQuestionnaire").innerHTML = "thankyou";
+                     return;
+                     }
                 var score = 0;
                 if (q1 == "yes") { score++; }
                 if (q2 == "yes") { score+=2; }
                 
                 if (score == 0 ) {
-                    swal("ไม่ป่วยเป็นโรคซึมเศร้า","","success");
+                    swal("ไม่เป็นโรคซึมเศร้า","แปลผลสำเร็จ","success");
                 } 
                 else{
-                    swal("มีแนวโน้มที่จะเป็นโรคซึมเศร้า","กรุณาทำแบบประเมิน 9Q เป็นขั้นต่อไป ","success")
+                    swal("“มีแนวโน้มที่จะเป็นโรคซึมเศร้า”","แปลผลสำเร็จ","success")
                 
                 }
             document.getElementById("processQuestionnaire").innerHTML = "thankyou";
